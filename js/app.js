4b42194920459d811720127bf96a22929d8f8ffc
lbunrith <lbunrith@prosper.com>
2016-08-14 11:38:20 -0700

    refactor html and js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
-angular.module('app', [])
+angular.module('app', ['ngSanitize'])
 
-.controller("appCtrl", ['$scope',function($scope) {
+.controller("appCtrl", ['$scope', 'questionAnswer', function($scope, questionAnswer) {
+  $scope.listQuestions = questionAnswer.list;
 }])
 
 .directive("toggleAnswer", function() {
@@ -21,5 +22,22 @@ angular.module('app', [])
       });
     }
   };
+})
+.constant("questionAnswer", {
+  list: [
+    {
+      question: "What is Javascript ?",
+      answer: "Javascript is a client-side scripting language that can be inserted into HTML pages and is understood by web browsers."
+    },
+    {
+      question: "What are JavaScript types ?",
+      answer: "<strong>Following are the JavaScript types:</strong><ul><li>Number</li><li>String</li><li>Boolean</li><li>Function</li><li>Object</li><li>Null</li><li>Undefined</li></ul>"
+    },
+    {
+      question: "What is the use of isNaN function ?",
+      answer: "isNaN function returns true if the argement is not a number otherwise it is false."
+    }
+  ]
+
 })
 ;
